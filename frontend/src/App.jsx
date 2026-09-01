0a179057213f490da69586e4889464362391d7e9
abdirashid-bx <cabdirashidbashirxasan@gmail.com>
2026-09-01 14:00:33 -0400

    hello

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import Privy from "./privy";
 import Termsand from "./Termsand";
 import Contactus from "./Contactus";
 import StructuredData from "./StructuredData";
-
-const BACKEND_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
 export default function App() {
   return (
     <>
